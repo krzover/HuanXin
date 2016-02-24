db5f3084bebc989b11bb6815d22687aa07068f83
krzover <krzover@163.com>
2016-02-24 19:15:04 +0800

    创建项目

---
+++ ChatXin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatXin
+//
+//  Created by Zlocal on 16/2/24.
+//  Copyright © 2016年 Zlocal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChatXin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChatXin
+//
+//  Created by Zlocal on 16/2/24.
+//  Copyright © 2016年 Zlocal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
